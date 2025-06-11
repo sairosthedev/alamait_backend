c634ac5a98f5c889f38a137546b2f5eac488abdf
kudzaicindy <165305935+kudzaicindy@users.noreply.github.com>
2025-06-11 10:11:25 +0200

    again

--- src/app.js
+++ src/app.js
@@ -110,13 +110,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from uploads directory
-app.use('/uploads', (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) {
-    return res.status(401).json({ message: 'Authentication required' });
-  }
-  next();
-}, express.static(path.join(__dirname, '../uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 // Basic route for root path
 app.get('/', (req, res) => {
